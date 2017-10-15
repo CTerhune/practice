6960615f60f6864950273658c3a6299278582be0
CTerhune <31664831+CTerhune@users.noreply.github.com>
2017-10-15 16:58:21 -0400

    Add files via upload

---
+++ wetmeat.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+int main(void) {
+int name1;
+	scanf("%d" , &name1);
+		if(name1 == Gerzbek) {
+			printf("Best Rust clan leader N/A\n");
+		}	
+		if(name1 == Misty) {
+			printf("Stuck up twat\n");
+		}
+		if(name1 == Conner) {
+			printf("Best PvPer N/A\n");
+		}
+		if(name1 == Beanus) {
+			printf("Ultimate script God, never to be challenged\n");
+		}
+		if(name1 == Mossy) {
+			printf("Whiney bitch\n");
+		}
+		if(name1 == Spoogle) {
+			printf("Great lad\n");
+		}
+		if(name1 == iZombie) {
+			printf("Gay\n");
+		}
+		if(name1 == OrangeChickn) {
+			printf("Owner of HellYeahHacks Inc.\n");
+		}
+		if(name1 == Zarcy) {
+			printf("Stuck up mormon\n");
+		}
+		if(name1 == Hooli) {
+			printf("High as a kite\n");
+		}
+		if(name1 == Nikkim) {
+			printf("Geek Squad employee on the month!\n");
+		}
+		if(name1 == AimFace) {
+			printf("Doesn't know what a condom is...\n");
+		}
+return 0;
+}
