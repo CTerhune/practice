18d28d6ad9fa336e9c4a932642d0b81c0da5d77f
CTerhune <31664831+CTerhune@users.noreply.github.com>
2017-11-16 12:41:07 -0500

    Add files via upload

---
+++ asciihw.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void) {
+
+	char letter1, letter2, letter3, letter4, letter5;
+	scanf("%c", &letter1);
+	scanf("%c", &letter2);
+	scanf("%c", &letter3);
+	scanf("%c", &letter4);
+	scanf("%c", &letter5);
+
+	letter1 = letter1 + 5;
+	
+
+
+	return 0;
+}
