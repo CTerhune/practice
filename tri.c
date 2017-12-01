7ce84d04ab696f03de2da8363b5df64994bda525
CTerhune <31664831+CTerhune@users.noreply.github.com>
2017-11-30 23:18:36 -0500

    Add files via upload

---
+++ tri.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void) {
+	int i, j, rows;
+	
+	printf("Enter the number of rows desired.\n");
+	scanf("%d", &rows);
+	
+
+	for(i=1; i<=rows; i++) {
+		for(j=1; j<=i; j++) {
+		printf("*");
+		}
+	printf("\n");
+	}
+	return 0;
+}
