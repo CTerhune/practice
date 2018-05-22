1728a51799b345ae92af05916d34b28b456f77c5
CTerhune <31664831+CTerhune@users.noreply.github.com>
2018-05-22 09:18:10 -0400

    Add files via upload

--- robot.h
+++ robot.h
@@ -14,8 +14,28 @@ class robot {
 		void setName();
 		void setHealth();
 
-	private:
+	protected:
 		string name;
 		int health;
 		int dmg;
+};
+//add 3 health, worth $15
+class tank:public robot {
+	public:
+		tank(int init_dmg);
+	protected:
+		void extraHealth(int health_add);
+};
+//add 3 damage, worth $15
+class mage:public robot {
+		mage(int init_dmg);
+	protected:
+		void extraDmg(int dmg_add);
+};
+//add 3 health and 3 dmg, worth $25
+class chad:public robot {
+		chad(int init_dmg);
+	protected:
+		void extraDmg(int dmg_add);
+		void extraHealth(int health_add);
 };
